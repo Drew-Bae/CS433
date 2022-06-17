60cff33a65b2b94c05783c6504b9265fcfb403ac
Jaeseo Bae <jae912@uab.edu>
2022-06-17 16:22:13 -0500

    #1 figured out getline #2 before i implement find_str in main

--- CS433/HW_1.c
+++ CS433/HW_1.c
@@ -1,21 +1,52 @@
+#include <string.h>
 #include <stdio.h>
-/* Test */
+#include <stdlib.h>
 
+/* Contains find_Str */
+void find_str(char const *str, char const *substr)
+{
+    char *pos = strstr(str, substr);
+    if (pos)
+    {
+        printf("found the string '%s' in '%s' at position: %ld\n", substr, str, pos - str);
+    }
+    else
+    {
+        printf("the string '%s' was not found in '%s'\n", substr, str);
+    }
+}
+
+/* Contains getting the arguments and getline */
 int main(int argc, char *argv[])
 {
+    /* These are for the command line */
+    int keyword;
     int counter;
-    printf("Program name is: %s \n", argv[0]);
+    /* These are for the getline */
+    char *line = NULL;
+    size_t maxlen = 0;
+    ssize_t n;
+
     if (argc == 1)
     {
         printf("There are no extra Command Line Arguments. \n");
     }
     if (argc >= 2)
     {
-        printf("Number of arguments passed: %d. \n", argc);
-        printf("These are the arguments put in. \n");
-        for (counter = 0; counter < argc; counter++)
+        /* second part */
+        while ((n = getline(&line, &maxlen, stdin)) > 0)
         {
-            printf("argv[%d]: %s \n", counter, argv[counter]);
+            printf("Line read:\n[%s]", line);
+        }
+        free(line);
+
+        /* first part */
+        printf("Number of arguments passed: %d. \n", argc);
+        printf("Here is the number of times each keyword appears: \n");
+        for (keyword = 1; keyword < argc; keyword++)
+        {
+            /* if keyword then this function here */
+            printf("%s: %d \n", argv[keyword], counter);
         }
     }
     return 0;
