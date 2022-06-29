5b62b90f8ab537b4ecd7f15c73f1a499e4cfdf4c
Jaeseo Bae <jae912@uab.edu>
2022-06-28 23:03:09 -0500

    lab3.1.c

--- Lab3/lab3.1.c
+++ Lab3/lab3.1.c
@@ -5,28 +5,38 @@
 /*
 Instructions to compile and run the program:
 0.) Enter Vulcan
-1.) gcc -o lab3 lab3.c
-2.) ./lab3
+1.) gcc -o lab3.1 lab3.1.c
+2.) ./lab3.1
 3.) follow the prompts
 4.) winner winner chicken dinner
 */
 
 /* Function Declaration */
-void sortArray(char word[], int size);
-void displayArray(char word[], int size);
+void sortArray(char *word[], int size);
+void displayArray(char *word[], int size);
 
 /* Main */
 int main(void)
 {
-    char str[46];
-    char *word;
-    int size;
-    printf("Please enter one word: \n");
-    scanf("%s", &str);
+    char str[5][46];
+    char **word;
+    int size, i;
+    printf("Please enter 5 words: \n");
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%46s", str[i]);
+    }
     /* Dynamic memory allocation for array */
-    word = (char *)malloc(strlen(str) + 1);
-    strcpy(word, str);
-    size = strlen(word);
+    word = malloc(5 * sizeof(char *));
+    for (i = 0; i < 5; i++)
+    {
+        word[i] = malloc(5 * sizeof(char));
+    }
+    for (i = 0; i < 5; i++)
+    {
+        strcpy(word[i], str[i]);
+    }
+    size = 5;
     /* Function call/invocation */
     sortArray(word, size);
     displayArray(word, size);
@@ -35,14 +45,14 @@ int main(void)
 }
 
 /* Function definition */
-void sortArray(char word[], int size)
+void sortArray(char *word[], int size)
 {
     int currLoc, i;
-    char temp;
+    char *temp;
     for (i = 1; i < size; i++)
     {
         currLoc = i;
-        while (currLoc > 0 && word[currLoc - 1] > word[currLoc])
+        while (currLoc > 0 && strcmp(word[currLoc], word[currLoc - 1]) < 0)
         {
             temp = word[currLoc - 1];
             word[currLoc - 1] = word[currLoc];
@@ -52,14 +62,14 @@ void sortArray(char word[], int size)
     }
 }
 
-void displayArray(char word[], int size)
+void displayArray(char *word[], int size)
 {
     int i;
     printf("\nSorted array is: ");
     printf("[");
     for (i = 0; i < size; i++)
     {
-        printf("%c, ", word[i]);
+        printf("%s, ", word[i]);
     }
     printf("%s]", word[size]);
 }
